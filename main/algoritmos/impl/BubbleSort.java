196d96b99c19a8274dec501e351b770703ac8ded
Alexsandro <alexsandro.sousa12366@gmail.com>
2023-09-02 16:43:56 -0300

    1 commit

---
+++ main/algoritmos/impl/BubbleSort.java
@@ -0,0 +1,27 @@
+package algoritmos.impl;
+
+//@Habilitado
+public class BubbleSort {
+    public BubbleSort(boolean impressao) {
+//        super(impressao);
+    }
+
+    protected  int[] executarAlgoritmoSort(int[] array) {
+        int n = array.length;
+        int temp = 0;
+
+        for (int i = 0; i < n; i++) { // comparando aos pares, adjacentes
+            for (int j = 1; j < (n - i); j++) {
+                int x = array[j - 1];
+                int y = array[j];
+
+                if (x > y) {
+                    temp = x;
+                    array[j - 1] = y;
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
